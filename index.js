cf2b0823e01d7aabd7cfefe494977de7c01a8731
Vidushiii <vidutomar19@gmail.com>
2021-04-19 00:33:18 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-//const path = require('path');
+const path = require('path');
 const port = 8000;
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', function (req, res) {
+    return res.render('home', { title: "expressssss" });
+});
 
 app.listen(port, function (err) {
     if (err) {
